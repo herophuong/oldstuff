f4801231db6ea8724699654b23fe739c93bab10f
Lê Hoàng Phương <herophuong93@gmail.com>
2013-09-01 23:46:16 +0700

    add User module skeleton

---
+++ module/User/src/User/Controller/UserController.php
@@ -0,0 +1,24 @@
+<?php
+namespace User\Controller;
+
+use Zend\Mvc\Controller\AbstractActionController;
+use Zend\View\Model\ViewModel;
+
+class UserController extends AbstractActionController
+{
+    public function indexAction()
+    {
+    }
+
+    public function registerAction()
+    {
+    }
+
+    public function loginAction()
+    {
+    }
+
+    public function deleteAction()
+    {
+    }
+}
