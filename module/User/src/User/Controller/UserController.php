7e1af9d7a0a4c103f6e11d65de53a727b7230127
Lê Hoàng Phương <herophuong93@gmail.com>
2013-09-02 23:14:45 +0700

    now allow registration

--- module/User/src/User/Controller/UserController.php
+++ module/User/src/User/Controller/UserController.php
@@ -4,9 +4,32 @@ namespace User\Controller;
 use Zend\Mvc\Controller\AbstractActionController;
 use Zend\View\Model\ViewModel;
 use User\Form\RegisterForm;
+use Doctrine\ORM\EntityManager;
+use Doctrine\DBAL\DBALException;
+use User\Entity\User;
+use Zend\Crypt\Password\Bcrypt;
+use Zend\Mvc\Controller\Plugin\FlashMessenger;
 
 class UserController extends AbstractActionController
 {
+    /**
+     * @var Doctrine\ORM\EntityManager
+     */
+    protected $em;
+    
+    public function setEntityManager(EntityManager $em)
+    {
+        $this->em = $em;
+    }
+ 
+    public function getEntityManager()
+    {
+        if (null === $this->em) {
+            $this->em = $this->getServiceLocator()->get('doctrine.entitymanager.orm_default');
+        }
+        return $this->em;
+    }
+    
     public function indexAction()
     {
     }
@@ -15,9 +38,55 @@ class UserController extends AbstractActionController
     {
         $form = new RegisterForm();
         
-        return array(
+        $request = $this->getRequest();
+        
+        if ($request->isPost()) {
+            $user = new User();
+            
+            $form->setData($request->getPost());
+            
+            if ($form->isValid()) {
+                $formdata = $form->getData();
+                $data     = $user->getArrayCopy();
+                $bcrypt   = new Bcrypt();
+                $data['email']      = $formdata['email'];
+                $data['password']   = $bcrypt->create($formdata['password']);
+                $data['state']      = 1;
+                
+                $user->populate($data);
+                
+                try {
+                    $this->getEntityManager()->persist($user);
+                    $this->getEntityManager()->flush();
+                    
+                    $this->flashMessenger()->addSuccessMessage('Register successfully!');
+                    
+                    $form->setData(array()); // clear the form
+                } catch (DBALException $e) {
+                    switch ($e->getPrevious()->getCode()) {
+                        case 23000: // MySQL Duplicate Key
+                            $this->flashMessenger()->addErrorMessage('This email is already registered!');
+                            break;
+                        default:
+                            $this->flashMessenger()->addErrorMessage($e->getMessage());
+                            break;
+                    }
+                }
+            } else {
+                $this->flashMessenger()->addErrorMessage($form->getMessages());
+            }
+        }
+        
+        $return = array(
             'form' => $form,
+            'success_messages' => $this->flashMessenger()->getCurrentSuccessMessages(),
+            'error_messages' => $this->flashMessenger()->getCurrentErrorMessages(),
         );
+        
+        $this->flashMessenger()->clearCurrentMessagesFromNamespace(FlashMessenger::NAMESPACE_SUCCESS);
+        $this->flashMessenger()->clearCurrentMessagesFromNamespace(FlashMessenger::NAMESPACE_ERROR);
+        
+        return $return;
     }
 
     public function loginAction()
---
+++ module/User/src/User/Entity/User.php
@@ -0,0 +1,90 @@
+<?php
+namespace User\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+/**
+ * A user
+ *
+ * @ORM\Entity
+ * @ORM\Table(name="user")
+ * @property int    $user_id
+ * @property string $email
+ * @property string $display_name
+ * @property string $password
+ * @property int    $state
+ */
+class User
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer");
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $user_id;
+    
+    /**
+     * @ORM\Column(type="string")
+     */
+    protected $email;
+    
+    /**
+     * @ORM\Column(type="string")
+     */
+    protected $display_name;
+    
+    /**
+     * @ORM\Column(type="string")
+     */
+    protected $password;
+    
+    /**
+     * @ORM\Column(type="integer")
+     */
+    protected $state;
+    
+    /**
+     * Magic getter to expose protected properties.
+     *
+     * @param string $property
+     * @return mixed
+     */
+    public function __get($property) 
+    {
+        return $this->$property;
+    }
+ 
+    /**
+     * Magic setter to save protected properties.
+     *
+     * @param string $property
+     * @param mixed $value
+     */
+    public function __set($property, $value) 
+    {
+        $this->$property = $value;
+    }
+    
+    /**
+     * Convert the object to an array.
+     *
+     * @return array
+     */
+    public function getArrayCopy() 
+    {
+        return get_object_vars($this);
+    }
+ 
+    /**
+     * Populate from an array.
+     *
+     * @param array $data
+     */
+    public function populate($data = array()) 
+    {
+        $this->user_id = $data['user_id'];
+        $this->email = $data['email'];
+        $this->display_name = $data['display_name'];
+        $this->password = $data['password'];
+        $this->state = $data['state'];
+    }
+}
