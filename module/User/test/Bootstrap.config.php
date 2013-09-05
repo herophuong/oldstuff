315e8133512a3c93edfc1362db6fef2713aa381e
Lê Hoàng Phương <herophuong93@gmail.com>
2013-09-05 18:21:17 +0700

    user in-memory sqlite for faster access

--- module/User/test/Bootstrap.config.php
+++ module/User/test/Bootstrap.config.php
@@ -5,7 +5,7 @@ return array(
             'orm_default' => array(
                 'driverClass' => 'Doctrine\DBAL\Driver\PDOSqlite\Driver',
                 'params' => array(
-                    'path' => \UserTest\Bootstrap::findParentPath('module').'/../data/test.db',
+                    'memory' => true,
                 )
             )
         )
