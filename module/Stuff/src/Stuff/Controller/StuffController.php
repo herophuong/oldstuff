442acfcb09474592f1096c1640e528dc09f65260
piavgh <piavghoang@gmail.com>
2013-09-09 23:40:28 +0700

    modified Edit Stuff

--- module/Stuff/src/Stuff/Controller/StuffController.php
+++ module/Stuff/src/Stuff/Controller/StuffController.php
@@ -98,6 +98,72 @@ class StuffController extends AbstractActionController {
 	}
 	
 	public function editAction(){
+
+		$form = new AddStuffForm();
+		$form->get('submit')->setAttribute('value', 'Edit');
+		$user_id = (int) $this->params()->fromroute('user_id',0);
 		
+		if(!$user_id){
+			return $this->redirect()->toRoute('user',array('action' => 'register'));
+		}
+
+		$stuff_id = (int) $this->params()->fromroute('stuff_id',0);
+        if (!$stuff_id) {
+            return $this->redirect()->toRoute('stuff', array('action'=>'add'));
+        } 
+
+        $stuff = $this->getEntityManager()->find('Stuff\Entity\Stuff', $stuff_id);
+
+        $request = $this->getRequest();
+		
+		if($request->isPost()){
+			
+			$form->setData($request->getPost());
+			
+			if($form->isValid()){
+				$formdata = $form->getData();
+				$data = $stuff->getArrayCopy();
+				$data['stuff_name'] = $formdata['stuffname'];
+				$data['description'] = $formdata['description'];
+				$data['price'] = $formdata['price'];
+				$data['cat_id']= 1;
+				$data['user_id'] = $user_id;
+				$data['state'] = 0;
+				
+				$stuff->populate($data);
+				try{
+					$this->getEntityManager()->persist($stuff);
+					$this->getEntityManager()->flush();
+					// $this->flashMessenger()->addSuccessMessage("Your stuff has been edited successfully");
+					return $this->redirect()->toRoute('stuff',array('user_id' => $user_id,
+																	'action' => 'index',
+					));
+				}
+				catch(DBALException $e){
+					switch ($e->getPrevious()->getCode()) {
+                        default:
+                            $this->flashMessenger()->addErrorMessage($e->getMessage());
+                        break;
+					}
+				}
+			}
+			else {
+				foreach ($form->getMessages() as $message_array) {
+                    foreach ($message_array as $message) {
+                        $this->flashMessenger()->addErrorMessage($message);
+                    }
+                }
+			}
+				
+		}$return = array(
+            'form' => $form,
+            'success_messages' => $this->flashMessenger()->getCurrentSuccessMessages(),
+            'error_messages' => $this->flashMessenger()->getCurrentErrorMessages(),
+        );
+        
+        $this->flashMessenger()->clearCurrentMessagesFromNamespace(FlashMessenger::NAMESPACE_SUCCESS);
+        $this->flashMessenger()->clearCurrentMessagesFromNamespace(FlashMessenger::NAMESPACE_ERROR);
+        
+        return $return;
 	}
 }
