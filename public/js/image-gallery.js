b182db9dc2a5f0ee4c9e5458a74c464c6308a111
Lê Hoàng Phương <herophuong93@gmail.com>
2013-09-23 18:57:36 +0700

    add filter bar to home layout

--- public/js/image-gallery.js
+++ public/js/image-gallery.js
@@ -1,7 +1,9 @@
 $(document).ready(function () {
     
+    /* TOOLTIP */
     $("[rel=tooltip]").tooltip();
     
+    /* IMAGE POPUP */
     $("a.group").fancybox({
         'titleShow'     : false,
         openEffect : 'elastic',
@@ -11,6 +13,7 @@ $(document).ready(function () {
         closeClick : true,
     });
     
+    /* ISOTOPE */
     $container = $("#gallery-container");
     
     $container.isotope({
@@ -24,5 +27,13 @@ $(document).ready(function () {
             // update columnWidth to a percentage of container width
             masonry: { columnWidth: $container.width() / 4 }
         });
+    });
+    
+    /* FILTER BAR */
+    $("#category-select").on('change', function() {
+        $("#filter-category").val(jQuery(this).val());
+    });
+    $("#purpose-select > button").on('click', function() {
+        $("#filter-purpose").val(jQuery(this).data('value'));
     });
 });
