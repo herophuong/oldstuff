a4dd69b154bb6c4eb559b6a67ceb7c6823ea0128
Hoang Trinh Huy <piavghoang@gmail.com>
2013-09-30 22:18:06 +0700

    finish basic vote function

--- module/Vote/data/schema.sql
+++ module/Vote/data/schema.sql
@@ -1,6 +1,14 @@
 CREATE TABLE vote
 {
-	voted_user_id INT NOT NULL,
 	user_id INT NOT NULL,
-	ratescore FLOAT NOT NULL
+	voted_user_id INT NOT NULL,
+	ratescore INT NOT NULL,
+	PRIMARY KEY (user_id, voted_user_id)
+};
+
+CREATE TABLE userrate
+{
+	user_id INT PRIMARY KEY,
+	avgrate FLOAT,
+	numofvote INT
 };
